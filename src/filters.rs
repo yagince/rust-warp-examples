455bd4e509ab277af963a2d8e23a736abe2ba7af
yagince <straitwalk@gmail.com>
2021-02-07 00:46:36 +0900

    auth

--- src/filters.rs
+++ src/filters.rs
@@ -1,8 +1,9 @@
 use super::handlers;
+use serde_derive::{Deserialize, Serialize};
 use warp::Filter;
 
 pub fn api() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    hello().or(receive_json())
+    hello().or(receive_json()).or(with_auth_json())
 }
 
 pub fn hello() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -26,6 +27,67 @@ pub fn receive_json() -> impl Filter<Extract = impl warp::Reply, Error = warp::R
         .and(warp::post())
         .and(warp::body::json())
         .and_then(handlers::receive_json)
+}
+
+pub fn with_auth_json() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+{
+    warp::path!("with_auth" / "json")
+        .and(with_authenticate())
+        .and(warp::post())
+        .and(warp::body::json())
+        .and_then(handlers::with_auth)
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct User {
+    pub id: u64,
+    pub name: String,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("no auth header")]
+    NoAuthHeaderError,
+    #[error("invalid auth header")]
+    InvalidAuthHeaderError,
+}
+impl warp::reject::Reject for Error {}
+
+pub fn with_authenticate() -> impl Filter<Extract = (User,), Error = warp::Rejection> + Clone {
+    warp::header::headers_cloned().and_then(authorize)
+}
+
+async fn authorize(
+    headers: warp::http::HeaderMap<warp::http::HeaderValue>,
+) -> Result<User, warp::Rejection> {
+    match token_from_header(&headers) {
+        Ok(token) => {
+            if token != "hogehoge" {
+                return Err(warp::reject::custom(Error::InvalidAuthHeaderError));
+            }
+            Ok(User {
+                id: 0,
+                name: "hoge".to_owned(),
+            })
+        }
+        Err(e) => Err(warp::reject::custom(e)),
+    }
+}
+
+fn token_from_header(
+    headers: &warp::http::HeaderMap<warp::http::HeaderValue>,
+) -> Result<String, Error> {
+    let header = headers
+        .get(warp::http::header::AUTHORIZATION)
+        .ok_or_else(|| Error::NoAuthHeaderError)?;
+
+    let value = std::str::from_utf8(header.as_bytes()).map_err(|_| Error::NoAuthHeaderError)?;
+
+    if !value.starts_with("Bearer ") {
+        return Err(Error::InvalidAuthHeaderError);
+    }
+
+    Ok(value.trim_start_matches("Bearer ").to_owned())
 }
 
 #[cfg(test)]
@@ -81,6 +143,50 @@ mod tests {
         assert_json_eq!(
             serde_json::from_slice::<serde_json::Value>(res.body())?,
             body,
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_receive_json_invalid() -> anyhow::Result<()> {
+        let body = json!({ "hoge": "foo"});
+
+        let res = warp::test::request()
+            .method("POST")
+            .path("/receive/json")
+            .body(body.to_string())
+            .reply(&api())
+            .await;
+
+        assert_eq!(res.status(), 400);
+        assert_eq!(
+            res.body(),
+            "Request body deserialize error: missing field `data` at line 1 column 14".as_bytes()
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_with_auth() -> anyhow::Result<()> {
+        let body = json!({ "data": "foo"});
+
+        let res = warp::test::request()
+            .method("POST")
+            .header("Authorization", "Bearer hogehoge")
+            .path("/with_auth/json")
+            .body(body.to_string())
+            .reply(&api())
+            .await;
+
+        assert_eq!(res.status(), 200);
+        assert_eq!(
+            serde_json::from_slice::<serde_json::Value>(res.body())?,
+            json!({
+                "user": { "id": 0, "name": "hoge" },
+                "body": body,
+            }),
         );
 
         Ok(())
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,8 @@
 use serde_derive::{Deserialize, Serialize};
 use std::convert::Infallible;
 
+use super::filters::User;
+
 pub async fn hello(name: String) -> Result<impl warp::Reply, Infallible> {
     Ok(format!("Hello, {}!", name))
 }
@@ -18,4 +20,13 @@ pub async fn hello_json(name: String) -> Result<impl warp::Reply, Infallible> {
 
 pub async fn receive_json(body: HelloJson) -> Result<impl warp::Reply, Infallible> {
     Ok(warp::reply::json(&body))
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct WithAuthJson {
+    user: User,
+    body: HelloJson,
+}
+pub async fn with_auth(user: User, body: HelloJson) -> Result<impl warp::Reply, Infallible> {
+    Ok(warp::reply::json(&WithAuthJson { user, body }))
 }
